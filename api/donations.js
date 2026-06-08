ac5aebee9c1d9f974de4fd89eb5740062ffa8d80
myumelldy1 <myumelldy1@gmail.com>
2026-06-08 11:52:14 +0700

    Create donations.js

---
+++ api/donations.js
@@ -0,0 +1,31 @@
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const OWNER = process.env.GITHUB_OWNER;
+const REPO = process.env.GITHUB_REPO;
+
+export default async function handler(req, res) {
+    try {
+        const response = await fetch(
+            `https://api.github.com/repos/${OWNER}/${REPO}/contents/data/donations.json`,
+            {
+                headers: {
+                    Authorization: `Bearer ${GITHUB_TOKEN}`,
+                    Accept: "application/vnd.github+json"
+                }
+            }
+        );
+
+        const file = await response.json();
+
+        const content = Buffer.from(
+            file.content,
+            "base64"
+        ).toString("utf8");
+
+        res.status(200).json(JSON.parse(content));
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+}
